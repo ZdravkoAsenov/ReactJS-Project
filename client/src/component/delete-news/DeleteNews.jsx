e5163660d07b12be57335a2762e2f8c3bf009ebc
ZdravkoAsenov <zdravkoasenov2004@abv.bg>
2023-12-06 20:24:02 +0200

    Add delete news

--- client/src/component/delete-news/DeleteNews.jsx
+++ client/src/component/delete-news/DeleteNews.jsx
@@ -1,32 +1,28 @@
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import * as newsService from '../../services/newsService';
 import styles from './DeleteNews.module.css';
+import Path from '../../paths';
 
 const DeleteNews = () => {
-  const { id } = useParams();
+    const navigate = useNavigate();
+    const { id } = useParams();
 
-  const handleDelete = () => {
-    // Implement delete logic
-    newsService.delete(id)
-      .then(() => {
-        // Redirect to all news page after delete
-        // history.push('/all-news');
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+    const handleDelete = async () => {
+        await newsService.remove(id);
 
-  return (
-    <div>
-      <h2>Delete News</h2>
-      <p>Are you sure you want to delete this news item?</p>
-      <div className={styles.deleteActions}>
-        <button onClick={handleDelete}>Delete</button>
-        <Link to={`/news/${id}`} className={styles.cancelLink}>Cancel</Link>
-      </div>
-    </div>
-  );
+        navigate(Path.ListNews);
+    };
+
+    return (
+        <div>
+            <h2>Delete News</h2>
+            <p>Are you sure you want to delete this news item?</p>
+            <div className={styles.deleteActions}>
+                <button onClick={handleDelete}>Delete</button>
+                <Link to={`/news/${id}`} className={styles.cancelLink}>Cancel</Link>
+            </div>
+        </div>
+    );
 };
 
 export default DeleteNews;
